812150f701cd0263ce6e6bb3334935d90a1df0b1
slongurst <steve@hopster.tv>
2017-12-11 07:25:02 +0000

    Split spritey.js into several files, 1 per class
    I had been implementing everything in spritey.js for the Gumbler animations. Now each class is split into it's own file.

---
+++ src/spritey/gumbler_animator.js
@@ -0,0 +1,177 @@
+'use strict';
+
+/**
+ * <p>
+ *     The class responsible for animating gumbler sprites
+ * </p>
+ * @class
+ * @extends cc.Class
+ */
+aq.spritey.GumblerAnimator = cc.Class.extend (/** @lends cc.Class# */{
+
+   gumbler: null,
+
+   current_anim_keys: null,
+
+   transition_to: null,
+
+   ctor: function (gumbler) {
+      this.gumbler = gumbler;
+   },
+
+   getGumbler: function () {
+       return this.gumbler;
+   },
+
+   getCurrentAnimKeys: function () {
+       return this.current_anim_keys;
+   },
+
+   isTransitioning: function () {
+       return (this.transition_to !== null);
+   },
+
+   _dispatchEvent: function (name) {
+       var self = this;
+
+       cc.eventManager.dispatchCustomEvent (aq.spritey.GumblerAnimator.EVENT, {
+          'name': name,
+          'gumbler': self.gumbler
+       });
+   },
+
+   initTransitions: function () {
+       var self = this;
+
+       let data = self.gumbler.getUserData ();
+
+       let anim = data.anim;
+
+       let num_transitions = anim.keys.length;
+
+       // Combine the global transitions with the current
+       let global_keys = aq.spritey.GumblerAnimator.global_keys;
+       let num_global_transitions = global_keys.length;
+
+       // Stash a list of all the anim transitions
+       self.current_anim_keys = [];
+       for (let i = 0; i < num_transitions; i++) {
+          self.current_anim_keys.push (anim.keys [i]);
+       }
+       for (let i = 0; i < num_global_transitions; i++) {
+          self.current_anim_keys.push (global_keys [i]);
+       }
+
+       self._dispatchEvent ('initTransition');
+   },
+
+   handleTransition: function () {
+       var self = this;
+
+       let sprite_frame_index = self.gumbler.getUserData ().frameIndex;
+
+       if (self.transition_to) {
+          if (self.transition_to.now || self.transition_to.on_frame === sprite_frame_index) {
+
+             let to_anim = self.transition_to.transition.to_anim;
+             let to_frame = self.transition_to.transition.getFrame ();
+             let offset = self.transition_to.transition.getOffset ();
+
+             self.gumbler._setSpriteAnim (to_anim, to_frame, offset);
+
+             self.initTransitions ();
+
+             self.transition_to = null;
+          }
+       }
+   },
+
+   startTransition: function (key) {
+       var self = this;
+
+       if (self.transition_to) {
+          return true;
+       }
+
+       // The current sprite frame
+       let sprite_frame_index = self.gumbler.getUserData ().frameIndex;
+
+       let transition_to = null;
+       let transition_on_frame = sprite_frame_index;
+       let transition_now = false;
+
+       if (key.all) {
+          // Trigger the transition straight away
+          transition_to = key.transitions [0];
+          transition_now = true;
+       } else {
+          // The transition triggers on a certain frame
+         if (key.transitions [sprite_frame_index].to_anim) {
+            // We're on that frame, trigger it now
+            transition_to = key.transitions [sprite_frame_index];
+            transition_now = true;
+         } else {
+            // Loop over the transitions array until we find the next labelled transition frame
+            // This loops over the --> entries in the state_trans_key() entries
+            let num_frames = key.transitions.length;
+            let start = sprite_frame_index;
+            let end = (sprite_frame_index + num_frames - 1) % num_frames;
+            for (let i = start; i !== end; i = (i + 1) % num_frames ) {
+               if (key.transitions [i].to_anim) {
+                  transition_to = key.transitions [i];
+                  transition_on_frame = i;
+                  break;
+               }
+            }
+         }
+      }
+
+       if (transition_to !== null) {
+          self.transition_to = {
+             transition: transition_to,
+             on_frame: transition_on_frame,
+             now: transition_now,
+             key: key
+          };
+       }
+
+       if (self.transition_to) {
+          return true;
+       }
+
+       return false;
+   }
+
+});
+
+aq.spritey.GumblerAnimator.global_keys = [];
+aq.spritey.GumblerAnimator.initGlobalKeys = function () {
+    let global_keys = aq.spritey.GumblerAnimator.global_keys;
+
+    let object_list = aq.spritey.object_list;
+    for (let o in object_list) {
+       if (object_list [o]) {
+          let object = object_list [o];
+          if (object.type () === 'Key') {
+             global_keys.push (object);
+          }
+       }
+    }
+};
+
+aq.spritey.GumblerAnimator.getGlobalTransitions = function () {
+   return aq.spritey.GumblerAnimator.global_keys;
+};
+
+aq.spritey.GumblerAnimator.EVENT = 'gumbler_animator';
+
+aq.spritey.animator = function (gumbler) {
+
+   if (aq.spritey.GumblerAnimator.global_keys.length === 0) {
+      aq.spritey.GumblerAnimator.initGlobalKeys ();
+   }
+
+   return new aq.spritey.GumblerAnimator (gumbler);
+};
+
+aq.spritey.GumblerAnimator.create = aq.spritey.animator;
---
+++ src/spritey/sprite.js
@@ -0,0 +1,230 @@
+'use strict';
+
+var SPRITEY_ALPHA = 255;
+
+/**
+ * <p>
+ *     A custom cc.Sprite subclass with AquaStax functionality.
+ * </p>
+ * @class
+ * @extends cc.Sprite
+ */
+aq.spritey.Sprite = cc.Sprite.extend(/** @lends aq.spritey.Sprite# */{
+   debugRect: null,
+   ctor: function () {
+      var self = this;
+
+      self._super ();
+
+      if (aq.config.SPRITEY_DEBUG) {
+         self.debugRect = new cc.DrawNode ();
+         self.addChild (self.debugRect);
+         self.scheduleUpdate ();
+      }
+
+      if (SPRITEY_ALPHA !== 255) {
+         self.setOpacity (SPRITEY_ALPHA);
+      }
+   },
+
+   update: function (dt) {
+       // jshint unused:false
+       var self = this;
+
+       if (aq.config.SPRITEY_DEBUG) {
+          self._updateDebugRects ();
+       }
+   },
+
+   _updateDebugRects: function () {
+       var self = this;
+
+       if (!self.debugRect) {
+          return;
+       }
+
+       let size = self.getContentSize ();
+       self.debugRect.clear ();
+       self.debugRect.drawRect (cc.p (0,0), cc.p (size.width, size.height),
+                                         null, // fillcolor
+                                         1,    // line width
+                                         cc.color (128,128,0,SPRITEY_ALPHA));
+
+       let app = self.getAnchorPointInPoints ();
+       self.debugRect.drawRect (app, cc.p (app.x + 1, app.y + 1), cc.color (255,0,0,SPRITEY_ALPHA), 1, cc.color (255,0,0,SPRITEY_ALPHA));
+   },
+
+   /**
+    * Override from cc.Sprite.setSpriteFrame.
+    * Moves the sprite based on the new frame data, when the frame
+    * is set.
+    *
+    * @override
+    */
+   setSpriteFrame: function (newFrame) {
+       this._setSpriteFrame (newFrame, true);
+   },
+
+   /**
+    * Custom setSpriteFrame method that determines the frame index
+    * of the given frame, and optionally moves the sprite, since
+    * movement is tied to frames in the Spritey system.
+    */
+   _setSpriteFrame: function (newFrame, movement) {
+      if (!newFrame) {
+         return;
+      }
+
+      let spriteFrame = newFrame;
+      if (newFrame instanceof cc.AnimationFrame) {
+         spriteFrame = newFrame.getSpriteFrame ();
+      }
+
+      if (!spriteFrame) {
+         return;
+      }
+
+      if (typeof spriteFrame.flippedX !== 'undefined') {
+         this.setFlippedX (spriteFrame.flippedX);
+      } else {
+         this.setFlippedX (false);
+      }
+
+      // Determine the sprite frame index. This is a bit brute force, it could be optimised
+      let userData = this.getUserData ();
+      let frames = userData.animation.getFrames ();
+      for (let index = 0; index < frames.length; index++) {
+         if (newFrame === frames [index].getSpriteFrame ()) {
+            userData.frameIndex = index;
+            break;
+         }
+      }
+
+      cc.Sprite.prototype.setSpriteFrame.call (this, newFrame);
+
+      // Need to update the sprite anchor based on the frame centre
+      this.setFrameAnchor ();
+
+      // Move the sprite if necessary
+      if (movement) {
+         this.move ();
+      }
+   },
+
+   /**
+    * Custom method to set the frame of the sprite, from the
+    * animation name, frame index, and optional movement based
+    * on the frame.
+    */
+   setDisplayFrameWithFrameIndex: function (animationName, frameIndex, movement) {
+       var cache = cc.animationCache.getAnimation (animationName);
+       if (!cache){
+           cc.log (cc._LogInfos.Sprite_setDisplayFrameWithAnimationName);
+           return;
+       }
+       var animFrame = cache.getFrames () [frameIndex];
+       if (!animFrame){
+           cc.log (cc._LogInfos.Sprite_setDisplayFrameWithAnimationName_2);
+           return;
+       }
+       this._setSpriteFrame(animFrame.getSpriteFrame (), movement);
+   },
+
+   _getPointForFrame: function (anim_array_name, index) {
+       let cp = cc.p (0, 0);
+       let userData = this.getUserData ();
+       let points = userData.anim [anim_array_name];
+
+       if (!points || points.length === 0) {
+          return null;
+       }
+
+       if (typeof index === 'undefined') {
+          index = userData.frameIndex;
+       }
+
+       if (points.length === 1) {
+          cp = cc.p (points [0].x, points [0].y);
+       } else if (typeof points [index] !== 'undefined') {
+          cp = cc.p (points [index].x, points [index].y);
+       }
+
+       return cp;
+   },
+
+   /**
+    * Getter for frame specific mirror (horizontal flip) status
+    */
+   getMirrorForFrame: function (index) {
+       let userData = this.getUserData ();
+       if (typeof index === 'undefined') {
+          index = userData.frameIndex;
+       }
+
+       let image = userData.anim.frames [userData.frameIndex];
+       return image.mirror;
+   },
+
+   /**
+    * Getter for frame specific custom point data
+    */
+   getCustomPointForFrame: function (index) {
+       return this._getPointForFrame ('custom_points', index);
+   },
+
+   /**
+    * Getter for frame specific movement data
+    */
+   getMoveForFrame: function (index) {
+       return this._getPointForFrame ('moves', index);
+   },
+
+   /**
+    * Move the sprite, based on it's current frame, or with a given
+    * offset delta.
+    */
+   move: function (offset) {
+
+       let delta = cc.p (0, 0);
+       let scale = this.getScale ();
+       let position = this.getPosition ();
+
+       // Optional offset, used on animation transitions
+       if (offset) {
+          delta.x += offset.x;
+          delta.y += offset.y;
+       } else {
+          delta = this.getMoveForFrame ();
+       }
+
+       if (delta.x !== 0 || delta.y !== 0) {
+          delta.x *= scale;
+          delta.y *= scale;
+          position.x += delta.x;
+          position.y -= delta.y;
+          this.setPosition (position);
+       }
+   },
+
+   /**
+    * Set the anchor point of the sprite based on the current frame
+    */
+   setFrameAnchor: function () {
+       let userData = this.getUserData ();
+       let image = userData.anim.frames [userData.frameIndex];
+       if (image.center) {
+          let cc_sprite_frame = cc.spriteFrameCache.getSpriteFrame (image.filename);
+          let size = cc_sprite_frame.getOriginalSizeInPixels ();
+          let cx = image.center.x;
+          let cy = image.center.y;
+          let ax = (size.width === 0) ? 0 : cx / size.width;
+          let ay = (size.height === 0) ? 0 : 1.0 - (cy / size.height);
+          if (image.mirror) {
+             ax = 1.0 - ax;
+          }
+          this.setAnchorPoint (ax, ay);
+       } else {
+          this.setAnchorPoint (0.5, 0.0);
+       }
+   }
+});
